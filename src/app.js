a3207cec0f0860e6fde29226c8d4118804c8b6a3
taisoliva <tais15oliva@gmail.com>
2023-04-14 08:52:41 -0300

    Using async/await

--- src/app.js
+++ src/app.js
@@ -9,21 +9,19 @@ const PORT = 5000;
 const { MongoClient } = mongodb;
 
 
-
 app.use(express.json())
 app.use(cors())
 dotenv.config()
 
 
 let db
-let isParticipants = false
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
 mongoClient.connect()
     .then(() => db = mongoClient.db())
     .catch((err) => console.log(err.message))
 
-app.post("/participants", (req, res) => {
+app.post("/participants", async (req, res) => {
 
     const { name } = req.body
 
@@ -32,30 +30,33 @@ app.post("/participants", (req, res) => {
         return res.status(422).send("Todos os campos são obrigatórios!")
     }
 
-    db.collection("participants").findOne({ name: name })
-        .then((teste) => {
-            if (teste !== null) {
-                return res.status(409).send("Este nome já esta sendo usado")
-            } else {
-                const time = dayjs().format("HH:mm:ss")
-                const newParticipant = { name: name, lastStatus: Date.now() }
-                const newMessage = { from: name, to: "Todos", text: "entra na sala...", type: "status", time: time }
-                db.collection("participants").insertOne(newParticipant)
-                    .then(() => {
-                        db.collection("messages").insertOne(newMessage)
-                            .then(() => { res.status(201).send("Participante adicionado!") })
-                            .catch((err) => res.status(500).send(err.message))
-                    })
-                    .catch((err) => { err.message })
-            }
-        })
-        .catch((err) => res.send(err.message))
+    try {
+        const isParticipant = await db.collection("participants").findOne({ name: name })
+        if (isParticipant !== null) return res.status(409).send("Este nome já esta sendo usado")
 
 
+        const time = dayjs().format("HH:mm:ss")
+        const newParticipant = { name: name, lastStatus: Date.now() }
+        const newMessage = { from: name, to: "Todos", text: "entra na sala...", type: "status", time: time }
+
+
+         await db.collection("participants").insertOne(newParticipant)
+         await db.collection("messages").insertOne(newMessage)
+       
+
+        return res.status(201).send("Participante adicionado!")
+
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
 
 
 
 })
+
+
+
+
 
 app.get("/participants", (req, res) => {
 
